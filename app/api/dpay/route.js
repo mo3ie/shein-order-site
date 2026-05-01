d3763a9e8a839b89e44e9fa95eaa19428138ea74
mo3ie <jumaalorfi@gmail.com>
2026-05-01 17:46:59 +0200

    fix dpay

--- app/api/dpay/route.js
+++ app/api/dpay/route.js
@@ -1,7 +1,6 @@
 export async function POST(req) {
 
-  console.log("🔥 TOKEN:", process.env.DPAY_TOKEN);
-console.log("🔥 MODE:", process.env.DPAY_MODE);
+ 
 
   try {
     const body = await req.json();
@@ -56,6 +55,10 @@ console.log("🔥 MODE:", process.env.DPAY_MODE);
     }
 
     console.log("📦 FINAL PAYLOAD:", payload);
+
+     console.log("🔥 TOKEN:", process.env.DPAY_TOKEN);
+console.log("🔥 MODE:", process.env.DPAY_MODE);
+
 
     const baseUrl =
       process.env.DPAY_MODE === "production"
